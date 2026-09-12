ae7656687a3126d9378d99bf60ac6df1d8df8fd3
Khris H. <contact@devkhris.com>
2026-09-12 17:29:45 -0600

    feat: normalized test for coverage

--- public/index.php
+++ public/index.php
@@ -10,6 +10,18 @@ use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
 
+$app->add(function (Request $request, callable $handler): Response {
+    $response = $handler->handle($request);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+$app->options('/{routes:.+}', function (Request $request, Response $response): Response {
+    return $response;
+});
+
 $app->get('/health', function (Request $request, Response $response): Response {
     $payload = json_encode(['message' => 'API is running']);
     $response->getBody()->write($payload);
